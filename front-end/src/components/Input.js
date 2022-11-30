14cca16721ac3fc126072bcbe184e2babc597fb4
Guilherme Axhcar <guilhermeaxhcar@gmail.com>
2022-11-30 18:46:28 -0300

    refactor: desestrutura dados para legibilidade

--- front-end/src/components/Input.js
+++ front-end/src/components/Input.js
@@ -9,7 +9,7 @@ function GenericInput({ testId, type, input, name, placeholder, setter }) {
       value={ input }
       fieldname={ name }
       placeholder={ placeholder }
-      onChange={ (event) => setter(event.target.value) }
+      onChange={ ({ target }) => setter(target.value) }
     />
   );
 }
