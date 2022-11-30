b4ef8d08c349d39ec24e6fd21eedc87217d6a7be
gralmeidan <gralmeidan@gmail.com>
2022-11-30 15:24:55 -0400

    Added error handling middleware

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,11 @@
+require('express-async-errors');
 const express = require('express');
+const handleError = require('../middlewares/handleErrorMiddleware');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use(handleError);
 
 module.exports = app;
