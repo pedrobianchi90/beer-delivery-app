232f8ba580d5a84999bfcaf9562b7acf286016df
clever-junior <tk_clever.junior@outlook.com>
2022-11-30 17:26:58 -0400

    feat: add bodyparser

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,11 +2,13 @@ require('express-async-errors');
 const express = require('express');
 const handleError = require('../middlewares/handleErrorMiddleware');
 const loginRoute = require('../routes/loginRoute');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
+app.use(bodyParser.json());
 app.use(handleError);
 app.use(loginRoute);
 
