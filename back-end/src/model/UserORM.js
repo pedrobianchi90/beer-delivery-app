3f352f8e3ae0a64572cd981957a284ac029fad48
clever-junior <tk_clever.junior@outlook.com>
2022-12-01 15:03:58 -0400

    feat: add register route - add register tests - add register validations - add cfunctions to UserORM

--- back-end/src/model/UserORM.js
+++ back-end/src/model/UserORM.js
@@ -1,12 +1,24 @@
 const { User } = require('../database/models');
 
+const create = async ({ email, password, name }) => {
+  const id = Math.floor((Date.now() * Math.random()) / 10000);
+  
+  const user = await User.create({ id, email, password, name, role: 'customer' });
+  
+  return user;
+};
+
 const findByPk = async (id) => User.findByPk(id);
+
+const findByEmail = async (email) => User.findOne({ where: { email } });
 
 const findByEmailAndPassword = async (email, password) => (
   User.findOne({ where: { email, password } }));
 
 const UserORM = {
+  create,
   findByPk,
+  findByEmail,
   findByEmailAndPassword,
 };
 
--- back-end/src/service/validation/userSchema.js
+++ back-end/src/service/validation/userSchema.js
@@ -1,19 +1,41 @@
 const Joi = require('joi');
 
+const ALL_FIELDS_REQUIRED = 'All fields must be filled';
+
 const loginSchema = Joi.object({
   email: Joi.string()
     .email()
     .required()
     .messages({
-      'any.required': 'All fields must be filled',
+      'any.required': ALL_FIELDS_REQUIRED,
     }),
   password: Joi.string()
     .required()
     .messages({
-      'any.required': 'All fields must be filled',
+      'any.required': ALL_FIELDS_REQUIRED,
+    }),
+});
+
+const userRegisterSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .messages({
+      'any.required': ALL_FIELDS_REQUIRED,
+    }),
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({
+      'any.required': ALL_FIELDS_REQUIRED,
+    }),
+  password: Joi.string()
+    .required()
+    .messages({
+      'any.required': ALL_FIELDS_REQUIRED,
     }),
 });
 
 module.exports = {
   loginSchema,
+  userRegisterSchema,
 };
