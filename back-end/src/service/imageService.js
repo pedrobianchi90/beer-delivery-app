6a0a685ca240d2186c753308fa0e470c1fa2020a
gralmeidan <gralmeidan@gmail.com>
2022-12-02 14:30:25 -0400

    Added tests for newly implemented routes

--- back-end/src/service/imageService.js
+++ back-end/src/service/imageService.js
@@ -1,13 +1,13 @@
-const { readFile } = require('fs/promises');
+const fs = require('fs/promises');
 const { join } = require('path');
 const RestError = require('../errors/RestError');
 
 const getImage = async (fileName) => {
   try {
-    const file = await readFile(join(__dirname, '../../public/', fileName));
+    const file = await fs.readFile(join(__dirname, '../../public/', fileName));
     return file;
   } catch (error) {
-    throw new RestError(404, 'File does not exist');
+    throw new RestError(404, 'File not found');
   }
 };
 
