769a3592d221689b68db7a637950a12d937f17a9
clever-junior <tk_clever.junior@outlook.com>
2022-12-06 14:45:59 -0400

    feat: add backend cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,14 @@
 require('express-async-errors');
+const cors = require('cors');
 const express = require('express');
 // const bodyParser = require('body-parser');
 const handleError = require('../middlewares/handleErrorMiddleware');
 const routes = require('../routes');
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
