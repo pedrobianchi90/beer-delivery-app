d4d90e6b00ec3182ba85b95995ce96f62edaf0d6
gralmeidan <gralmeidan@gmail.com>
2022-12-08 13:21:10 -0400

    Req30: Added OrderSummary component to display basic info about order

---
+++ front-end/src/components/OrderSummary.jsx
@@ -0,0 +1,66 @@
+import propTypes from 'prop-types';
+
+const padId = (id) => {
+  const padding = 4;
+  return String(id).padStart(padding, '0');
+};
+
+const formatDate = (saleDate) => saleDate.replace(
+  /(\d{4})-(\d{2})-(\d{2}).+/,
+  '$3/$2/$1',
+);
+
+function OrderSummary({
+  sale: {
+    id,
+    saleDate,
+    status,
+  },
+  children,
+  testIdPrefix,
+}) {
+  return (
+    <div>
+      <p>
+        <span
+          data-testid={
+            `${testIdPrefix}__element-order-details-label-order-id`
+          }
+        >
+          PEDIDO
+          {' '}
+          {padId(id)}
+        </span>
+        <span
+          data-testid={
+            `${testIdPrefix}__element-order-details-label-order-date`
+          }
+        >
+          {formatDate(saleDate)}
+        </span>
+        <span
+          data-testid={
+            `${testIdPrefix}__element-order-details-label-delivery-status`
+          }
+        >
+          {status}
+        </span>
+      </p>
+      <div>
+        { children }
+      </div>
+    </div>
+  );
+}
+
+OrderSummary.propTypes = {
+  sale: propTypes.shape({
+    id: propTypes.number.isRequired,
+    saleDate: propTypes.string.isRequired,
+    status: propTypes.string.isRequired,
+  }).isRequired,
+  children: propTypes.element.isRequired,
+  testIdPrefix: propTypes.string.isRequired,
+};
+
+export default OrderSummary;
--- front-end/src/pages/SellerOrderDetails.jsx
+++ front-end/src/pages/SellerOrderDetails.jsx
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import OrderSummary from '../components/OrderSummary';
 import { getSale } from '../service/saleRequests';
 
 function SellerOrderDetails() {
   const { id } = useParams();
   const [sale, setSale] = useState();
+  const testIdPrefix = 'seller_order_details';
 
   useEffect(() => {
     const fetchOrder = async () => {
       const response = await getSale(id);
       setSale(response.data);
-      console.log(response.data);
     };
     fetchOrder();
   }, [id]);
 
-  return <div>foo</div>;
+  return sale ? (
+    <main>
+      <OrderSummary sale={ sale } testIdPrefix={ testIdPrefix } />
+    </main>
+  ) : (
+    <p>loading</p>
+  );
 }
 
 export default SellerOrderDetails;
