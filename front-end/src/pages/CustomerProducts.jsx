c1ceccf81ae4692ecaf8bd87bff55e2c0cd51a60
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-12-08 10:50:54 -0300

    feat: botões dos cardProducts

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,23 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useDebugValue, useEffect, useState } from 'react';
 import { getAll } from '../service/products';
 import HeaderCustomer from '../components/HeaderCustomer';
 import ProductCard from '../components/ProductCard';
+import CartButton from '../components/CartButton';
+
 
 function CustomerProducts() {
   const [products, setProducts] = useState([]);
+  const [value, setValue] = useState([]);
+
+  const addToLocalStorage = ({ id, name, price, urlImage, quantity }) => {
+    const hasItem = value.findIndex((item) => item.id === id);
+    if (hasItem >= 0) {
+      value[hasItem].quantity += 1;
+      setValue([...value]);
+    } else {
+      setValue([...value, { id, name, price, urlImage, quantity }]);
+    }
+  };
+
+  const removeFromLocalStorage = (id) => {
+    const hasItem = value.findIndex((item) => item.id === id);
+    if (hasItem >= 0 && value[hasItem].quantity > 1) {
+      value[hasItem].quantity -= 1;
+      return setValue([...value]);
+    }
+    if (hasItem >= 0) {
+      value.splice(hasItem, 1);
+      return setValue([...value]);
+    }
+  };
+
+  const setOnLocalStorage = ({ id, name, price, urlImage, quantity }) => {
+    const hasItem = value.findIndex((item) => item.id === id);
+    if (hasItem >= 0 && quantity > 0) {
+      value[hasItem].quantity = quantity;
+      return setValue([...value]);
+    }
+    if (hasItem >= 0 && quantity <= 0) {
+      value.splice(hasItem, 1);
+      return setValue([...value]);
+    }
+    return setValue([...value, { id, name, price, urlImage, quantity }]);
+  };  
 
   useEffect(() => {
     async function getProducts() {
       const resp = await getAll();
       setProducts(resp.data);
-
-      // console.log(products);
     }
     getProducts();
   }, []);
 
   return (
-    <div>
+    <div className='cards'>
       <HeaderCustomer />
       <div>
         {products.length && products.map((product) => (
@@ -27,9 +63,16 @@ function CustomerProducts() {
             price={ product.price }
             img={ product.urlImage }
             name={ product.name }
+            addItem={ addToLocalStorage }
+            removeItem={ removeFromLocalStorage }
+            setItem={ setOnLocalStorage }            
+            qtd={ value.find((item) => item.id === product.id)?.quantity }
           />
         ))}
       </div>
+      <CartButton
+        total={ value.reduce((acc, v) => v.price * caches.quantity + acc, 0) }
+       />
     </div>
   );
 }
