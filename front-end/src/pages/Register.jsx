94d932e2d585f9942218d546c23d1f1f0cb67a9a
Pedro Bianchi <pedroedm@gmail.com>
2022-12-14 21:43:35 -0300

    feat: css de register

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -36,54 +36,71 @@ function Register() {
   };
 
   return (
-    <form>
-      <h1>Cadastro</h1>
-      <GenericInput
-        testId="common_register__input-name"
-        type="name"
-        input={ name }
-        name="name"
-        placeholder="Seu nome"
-        setter={ setName }
-        selector="name"
-        fieldName="Nome"
-      />
+    <div className="grid h-screen place-items-center bg-gray-300">
+      <form className="bg-yellow-300 shadow-xl rounded px-20 pt-20 pb-20 mb-10">
+        <h1
+          className="px-20 pb-10 font-xl text-2xl font-extrabold
+          text-gray-400 leading-tight"
+        >
+          Cadastro
+        </h1>
+        <GenericInput
+          className="shadow appearance-none border rounded w-full py-2 px-3
+          text-gray-400"
+          testId="common_register__input-name"
+          type="name"
+          input={ name }
+          name="name"
+          placeholder="Seu nome"
+          setter={ setName }
+          selector="name"
+          fieldName="Nome"
+        />
 
-      <GenericInput
-        testId="common_register__input-email"
-        type="email"
-        input={ email }
-        name="email"
-        placeholder="seu-email@site.com.br"
-        setter={ setEmail }
-        selector="email"
-        fieldName="Email"
-      />
+        <GenericInput
+          className="shadow appearance-none border rounded w-full py-2 px-3
+          text-gray-400 "
+          testId="common_register__input-email"
+          type="email"
+          input={ email }
+          name="email"
+          placeholder="seu-email@site.com.br"
+          setter={ setEmail }
+          selector="email"
+          fieldName="Email"
+        />
 
-      <GenericInput
-        testId="common_register__input-password"
-        type="password"
-        input={ password }
-        name="senha"
-        placeholder="********"
-        setter={ setPassword }
-        selector="senha"
-        fieldName="Senha"
-      />
+        <GenericInput
+          className="shadow appearance-none border rounded w-full py-2 px-3
+          text-gray-400 leading-tight"
+          testId="common_register__input-password"
+          type="password"
+          input={ password }
+          name="senha"
+          placeholder="********"
+          setter={ setPassword }
+          selector="senha"
+          fieldName="Senha"
+        />
 
-      <Button
-        dataTestId="common_register__button-register"
-        type="submit"
-        name="register"
-        disabled={ disabledBtn() }
-        text="Cadastrar"
-        onClick={ register }
-      />
-      <ErrorMessage
-        dataTest="common_register__element-invalid_register"
-        message={ response }
-      />
-    </form>
+        <Button
+          className="inline-block align-baseline font-bold px-10 py-2 mt-5 ml-14
+          text-yellow-500 bg-white rounded hover:bg-gray-200"
+          dataTestId="common_register__button-register"
+          type="submit"
+          name="register"
+          disabled={ disabledBtn() }
+          text="Cadastrar"
+          onClick={ register }
+        />
+        <ErrorMessage
+          className="inline-block align-baseline font-bold text-sm
+          text-gray-400 px-2 mt-5 ml-16"
+          dataTest="common_register__element-invalid_register"
+          message={ response }
+        />
+      </form>
+    </div>
   );
 }
 
